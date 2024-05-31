4e0dff7a40b920a19cbeeda09a01d38280fe110a
ArgiSanchez <sanchezargi@gmail.com>
2024-05-31 20:59:36 +0200

    Update arch_2.sh

--- arch_2.sh
+++ arch_2.sh
@@ -27,7 +27,7 @@ echo -e "127.0.0.1  localhost \n::1  localhost \n127.0.0.1  arh-argi.localdomain
 
 
 # install EFI boot and grub config
-grub-install —target=x86_64-efi —efi-directory=/boot/efi —bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 
