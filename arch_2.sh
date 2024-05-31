9e455f1987d5b106870a5aba9fb8e71283a78a84
ArgiSanchez <sanchezargi@gmail.com>
2024-05-31 21:49:57 +0200

    Update arch_2.sh

--- arch_2.sh
+++ arch_2.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# set de root password
 echo "set password for the root"
 passwd
 
@@ -15,7 +14,6 @@ passwd argi
 # add new user to sudoers -> uncoment %whell ALL=(ALL:ALL) ALL
 EDITOR=nano visudo
 
-
 # adjust some things
 ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 echo "arch-argi" >> /etc/hostname
@@ -26,12 +24,10 @@ locale-gen
 #nano /etc/locale.conf
 #LANG=en_US.UTF-8
 
-
 # install EFI boot and grub config
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
-
 
 # add btrfs label 
 btrfs filesystem label / ARCH
