da4896f5aa85b2f5216da192702907c76c7f0062
ArgiSanchez <sanchezargi@gmail.com>
2024-05-31 19:57:17 +0200

    Update arch_0.sh

--- arch_0.sh
+++ arch_0.sh
@@ -17,10 +17,14 @@ btrfs su cr /mnt/@home
 btrfs su cr /mnt/@cache
 btrfs su cr /mnt/@log
 btrfs su cr /mnt/@.snapshots
-mkdir /mnt/.snapshots
+
 
 # mount the btrfs volums
 umount /mnt
+mkdir /mnt/home
+mkdir -p /mnt/var/cache
+mkdir /mnt/var/log
+mkdir /mnt/.snapshots
 mount -o compress=zstd:1,subvol=@ /dev/vda2 /mnt
 mount -o compress=zstd:1,subvol=@home /dev/vda2 /mnt/home
 mount -o compress=zstd:1,subvol=@cache /dev/vda2 /mnt/var/cache
