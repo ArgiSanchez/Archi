c9c7cdf1cc38d6454fa2c5f1b15e196ab344c61c
ArgiSanchez <sanchezargi@gmail.com>
2024-06-01 12:36:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -34,17 +34,19 @@ mkfs.btrfs "${DISK}2" -f > /dev/null
 
 # Create subvolumes
 mount "${DISK}2" /mnt
-for subvol in @ @home @cache @log @snapshots; do
+for subvol in @ @tmp @home @cache @log @spool @snapshots; do
     btrfs su cr "/mnt/$subvol"
 done
 
 # Mount the btrfs subvolumes
 umount /mnt
 mount -o compress=zstd:1,subvol=@ "${DISK}2" /mnt
-mkdir -p /mnt/{home,var/cache,var/log,.snapshots}
+mkdir -p /mnt/{var/tmp,home,var/cache,var/log,var/spool,.snapshots}
+mount -o compress=zstd:1,subvol=@tmp "${DISK}2" /mnt/var/tmp
 mount -o compress=zstd:1,subvol=@home "${DISK}2" /mnt/home
 mount -o compress=zstd:1,subvol=@cache "${DISK}2" /mnt/var/cache
 mount -o compress=zstd:1,subvol=@log "${DISK}2" /mnt/var/log
+mount -o compress=zstd:1,subvol=@spool "${DISK}2" /mnt/var/spool
 mount -o compress=zstd:1,subvol=@snapshots "${DISK}2" /mnt/.snapshots
 
 # Mount the EFI partition
