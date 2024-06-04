27eb5441b10db6c66a91e8d7cb87f5bfcc357b34
ArgiSanchez <sanchezargi@gmail.com>
2024-06-04 11:55:55 +0200

    Update arch_2.sh

--- arch_2.sh
+++ arch_2.sh
@@ -56,17 +56,7 @@ systemctl enable NetworkManager
 # Label the Btrfs filesystem
 btrfs filesystem label / ARCH
 
-# Install printing support
-pacman -Sy --noconfirm cups cups-pdf 
-systemctl enable cups.service
-systemctl enable cups.socket
-# for graphical config install: system-config-printer
-
 # Install Snapper and configure snapshots
 pacman -Sy --noconfirm snapper
-# for graphical manangment install: btrfs-assistant
 
 echo "Installation complete. Please review the output for any errors."
-
-# if you want yo can install Hyperland from JaKooLit
-# git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git
